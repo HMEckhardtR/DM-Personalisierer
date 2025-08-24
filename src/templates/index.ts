c7b1935b610da3888571824b674accba56f8d461
Juan Miguel Eckhardt Rubio <alumno.486628@ies-azarquiel.es>
2025-08-24 09:19:16 +0000

    añadimos input para añadir links a los templates

--- src/templates/index.ts
+++ src/templates/index.ts
@@ -3,7 +3,7 @@ This is the password to unzip "NAME"
 
 xxxxx-xxxxx-xxxxx-xxxxx-xxxxx-xxxxx
 
-Link: https://
+Link: @link
 
 The post with the link will be published tomorrow at 9am CET.
 I hope you love it!
@@ -22,7 +22,7 @@ This is the password for Backup v2.1
 
 xxxxx-xxxxx-xxxxx-xxxxx-xxxxx-xxxxx
 
-Link: https://
+Link: @link
 
 If you have already downloaded the other 3 Backup files, The Mansion 48 and ToE2-4 111, you don't really need to download it.`;
 
